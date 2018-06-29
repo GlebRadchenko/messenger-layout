5cddcb99244a80c4e51665227092419d0087985a
Gleb Radchenko <gleb.radchenko@activewindow.dk>
2018-06-29 11:58:22 +0300

    Project setup

---
+++ MessengerLayout/MessengerKit/MessengerKit.h
@@ -0,0 +1,19 @@
+//
+//  MessengerKit.h
+//  MessengerKit
+//
+//  Created by Gleb Radchenko on 6/29/18.
+//  Copyright © 2018 Gleb Radchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MessengerKit.
+FOUNDATION_EXPORT double MessengerKitVersionNumber;
+
+//! Project version string for MessengerKit.
+FOUNDATION_EXPORT const unsigned char MessengerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessengerKit/PublicHeader.h>
+
+
---
+++ MessengerLayout/MessengerLayout/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MessengerLayout
+//
+//  Created by Gleb Radchenko on 6/29/18.
+//  Copyright © 2018 Gleb Radchenko. All rights reserved.
+//
+
+import UIKit
+
+open class ViewController: UIViewController {
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override open func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
